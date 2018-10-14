e2c3c28bfa006d2554e742edb19eb73644dae468
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-10-14 11:35:56 +0900

    14課題

--- mynews/resources/views/admin/profile/edit.blade.php
+++ mynews/resources/views/admin/profile/edit.blade.php
@@ -17,6 +17,48 @@
   <div class="row">
     <div class="col-md-8 mx-auto">
       <h2>Myプロフィール</h2>
+      <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        　　  <ul>
+            　　                  @foreach($errors->all() as $e)
+                                      <li>{{ $e }}</li>
+                                  @endforeach
+                              </ul>
+                          @endif
+                          <div class="form-group row">
+                              <label class="col-md-2" for="title">氏名（name）</label>
+                              <div class="col-md-10">
+                                  <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label class="col-md-2" for="title">性別（gender）</label>
+                              <div class="col-md-10">
+                                  <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label class="col-md-2" for="title">趣味（hobby）</label>
+                              <div class="col-md-10">
+                                  <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label class="col-md-2" for="body">自己紹介（introdaction）</label>
+                              <div class="col-md-10">
+                                  <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label class="col-md-2" for="title">画像</label>
+                              <div class="col-md-10">
+                                  <input type="file" class="form-control-file" name="image">
+                              </div>
+                          </div>
+                          {{ csrf_field() }}
+                          <input type="submit" class="btn btn-primary" value="更新">
+
     </div>
   </div>
 </div>
--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin','middleware' => 'auth'],function() {
  Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+ Route::post('profile/edit','Admin\ProfileController@update')->middleware('auth');
   Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
   Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
 });
