8f94b5d464fd67569bcd56da79983d86a5f7d3c6
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-10-16 23:11:14 +0900

    15課題

---
+++ mynews/database/migrations/2018_10_15_123424_create_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() //マイグレーション実行時に呼び出される関数.
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('hobby');
+            $table->string('introduction');
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() //ロールバック時に呼び出される関数.
+    {
+        Schema::dropIfExists('profile');
+    }
+}
