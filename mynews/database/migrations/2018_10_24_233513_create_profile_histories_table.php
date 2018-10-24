31db258f7dfed414613e0ca9667c992097efe6d6
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-10-24 23:56:22 +0900

    18課題Migration実行またエラー

--- mynews/database/migrations/2018_10_24_233513_create_profile_histories_table.php
+++ mynews/database/migrations/2018_10_24_233513_create_profile_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHistoriesTable extends Migration
+class CreateProfileHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('histories', function (Blueprint $table) {
+        Schema::create('profile_histories', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('news_id');
-            $table->string('edited_at');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('hobby');
+            $table->string('introduction');
 
             $table->timestamps();
         });
@@ -29,6 +31,6 @@ class CreateHistoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('histories');
+        Schema::dropIfExists('profile_histories');
     }
 }
