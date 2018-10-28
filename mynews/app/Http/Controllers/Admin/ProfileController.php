8537203fe689db6565bf9c3cd6729327f3edbd71
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-10-28 13:56:22 +0900

    updateAction訂正

--- mynews/app/Http/Controllers/Admin/ProfileController.php
+++ mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
 
+use App\ProfileHistories;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //以下を追記
-  public function edit()
+  public function edit(Request $request)
   {
     return view('admin.profile.edit');
   }
@@ -18,14 +21,32 @@ class ProfileController extends Controller
   public function update(Request $request)
   {
     $this->validate($request, Profile::$rules);
+    $news = Profile::find($request->id);
+    $news_form = $request->all();
+    if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
 
-    $news = new Profile;
-    $form = $request->all();
+
     unset($form['_token']);
-    
+    unset($news_form['image']);
+    unset($news_form['remove']);
+    $news->fill($news_form)->save();
+
     // データベースに保存する
      $news->fill($form);
      $news->save();
+
+                     // 以下を追記
+        $history = new ProfileHistories;
+        $history->profile_id= $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
 
     return redirect('admin/profile/edit');
