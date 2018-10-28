4017d3abad3a4d32f79e9e10434a98a53ed57841
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-10-28 18:25:15 +0900

    profileedit編集履歴設定エラー

--- mynews/resources/views/admin/profile/edit.blade.php
+++ mynews/resources/views/admin/profile/edit.blade.php
@@ -53,8 +53,8 @@
     <div class="col-md-4 mx-auto">
       <h2>編集履歴</h2>
       <ul class="list-group">
-        @if ($profile_form->profile_histories != NULL)
-        @foreach ($profile_form->profile_histories as $profilehistory)
+        @if ($form->profile_histories != NULL)
+        @foreach ($form->profile_histories as $profilehistory)
         <li class="list-group-item">{{ $profilehistory->edited_at }}</li>
         @endforeach
         @endif
