113d227de170970eb4a3543bf64498142e5c6d5e
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-11-03 22:22:46 +0900

    19課題news/profile作成中

---
+++ mynews/resources/views/news/profile.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+@section('title', 'ニュースの新規作成')
+
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mx-auto">
+      <h2>Myプロフィール</h2>
+      <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+
+        @if (count($errors) > 0)
+        <ul>
+          @foreach($errors->all() as $e)
+          <li>{{ $e }}</li>
+          @endforeach
+        </ul>
+        @endif
+        <div class="form-group row">
+          <label class="col-md-2" for="name">氏名（name）</label>
+          <div class="col-md-10">
+            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-2" for="gender">性別（gender）</label>
+          <div class="col-md-10">
+            <input type="radio" name="gender" value="male">男性
+            <input type="radio" name="gender" value="female">女性
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-2" for="hobby">趣味（hobby）</label>
+          <div class="col-md-10">
+            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-md-2" for="introduction">自己紹介（introduction）</label>
+          <div class="col-md-10">
+            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+          </div>
+        </div>
+        {{ csrf_field() }}
+        <input type="submit" class="btn btn-primary" value="更新">
+      </div>
+    </div>
+  </form>
+</ul>
+</div>
+</div>
+</div>
+</div>
+@endsection
