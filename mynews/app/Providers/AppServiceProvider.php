b3537916e964696c41309ba3cff35eb6e16fc6b6
matsushita <matsushita@matsushitakazuminoMacBook-ea.local>
2018-11-06 17:05:51 +0900

    herokuDB設定中

--- mynews/app/Providers/AppServiceProvider.php
+++ mynews/app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      // 以下を追記
+    if (\App::environment('production')) {
+    \URL::forceScheme('https');
+    }
+    }
     }
 
     /**
